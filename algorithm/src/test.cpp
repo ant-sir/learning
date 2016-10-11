04a771df4c3edc7dd5c788e1a3223da83d657561
zhuyanlei <zhuyanlei@iopencloud.com>
2016-10-11 20:21:16 +0800

    googletest
    Change-Id: If9e56b5a47d59baad01c55f975f4c8bbcaa61f78

--- algorithm/src/test.cpp
+++ algorithm/src/test.cpp
@@ -5,8 +5,25 @@
  *      Author: zyl
  */
 
-#include "gtest/gtest.h"
+#include <gtest/gtest.h>
 
+extern void bubble_sort(int arr[], int len);
 
+TEST(bubble_sort_test, bubble_sort_case)
+{
+	int arr[10] = {3, 6, 9, 1, 2, 7, 5, 4, 8, 10};
 
+	bubble_sort(arr, sizeof(arr));
+
+	EXPECT_EQ(1, arr[0]);
+	EXPECT_EQ(2, arr[1]);
+	EXPECT_EQ(3, arr[2]);
+	EXPECT_EQ(4, arr[3]);
+	EXPECT_EQ(5, arr[4]);
+	EXPECT_EQ(6, arr[5]);
+	EXPECT_EQ(7, arr[6]);
+	EXPECT_EQ(8, arr[7]);
+	EXPECT_EQ(9, arr[8]);
+	EXPECT_EQ(10, arr[9]);
+}
 
